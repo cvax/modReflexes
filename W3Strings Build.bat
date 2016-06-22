6175c33986a9f2c9ecfa9b47ee90d8ebdd103790
cvax <cvax@users.noreply.github.com>
2016-06-21 19:35:42 -0700

    Added French translations from alcask

--- W3Strings Build.bat
+++ W3Strings Build.bat
@@ -5,12 +5,15 @@ echo Encoding localization w3strings.........
 
 cd /d "%W3STRINGSENCODER%"
 call w3strings --encode %STRINGS%\locale.en.csv --id-space %W3STRINGSIDSPACE%
+call w3strings --encode %STRINGS%\locale.fr.csv --id-space %W3STRINGSIDSPACE%
 
 echo Removing debug Witcher Script files.........
 del %STRINGS%\locale.en.csv.w3strings.ws
+del %STRINGS%\locale.fr.csv.w3strings.ws
 
 echo Renaming to proper language names...........
 ren %STRINGS%\locale.en.csv.w3strings en.w3strings
+ren %STRINGS%\locale.fr.csv.w3strings fr.w3strings
 
 echo Generating other languages from English.....
 copy %STRINGS%\en.w3strings %PACKED%\ar.w3strings
@@ -20,7 +23,7 @@ copy %STRINGS%\en.w3strings %PACKED%\de.w3strings
 copy %STRINGS%\en.w3strings %PACKED%\en.w3strings
 copy %STRINGS%\en.w3strings %PACKED%\es.w3strings
 copy %STRINGS%\en.w3strings %PACKED%\esmx.w3strings
-copy %STRINGS%\en.w3strings %PACKED%\fr.w3strings
+copy %STRINGS%\fr.w3strings %PACKED%\fr.w3strings
 copy %STRINGS%\en.w3strings %PACKED%\hu.w3strings
 copy %STRINGS%\en.w3strings %PACKED%\it.w3strings
 copy %STRINGS%\en.w3strings %PACKED%\jp.w3strings
@@ -29,6 +32,7 @@ copy %STRINGS%\en.w3strings %PACKED%\pl.w3strings
 copy %STRINGS%\en.w3strings %PACKED%\ru.w3strings
 copy %STRINGS%\en.w3strings %PACKED%\zh.w3strings
 del %STRINGS%\en.w3strings
+del %STRINGS%\fr.w3strings
 
 
 echo W3Strings generation success.
