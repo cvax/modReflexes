ecd65b671ae81196e3c4c1712b26cec1026eaf20
cvax <cvax@users.noreply.github.com>
2016-07-03 14:02:56 -0700

    Added DLC files

--- BuildVariables.cmd
+++ BuildVariables.cmd
@@ -1,17 +1,20 @@
 set GAMEPATH=C:\Program Files\The Witcher 3 Wild Hunt
-set WITCHERBASEDIR=D:\Games\Witcher3\W3UncookedBnW
+set WITCHERBASEDIR=D:\Games\Witcher3\W3Uncooked_v122
 set MODKITPATH=C:\Program Files\The Witcher 3 Wild Hunt\Witcher 3 Mod Kit\bin\x64
 set W3STRINGSENCODER=C:\Program Files\The Witcher 3 Wild Hunt\Witcher 3 W3Strings Encoder
 
 set W3STRINGSIDSPACE=0956
 set NAME=modReflexes
+set DLCNAME=dlcreflexes
 
 set MODPATH=%~dp0
 set BUNDLES=%MODPATH%Bundles
 set MODDED=%BUNDLES%\1_Modded
+set DLC=%BUNDLES%\1b_DLC
 set COOKED=%BUNDLES%\2_Cooked
 set PACKED=%MODPATH%Compiled\%NAME%\content
 set PACKEDSCRIPTS=%MODPATH%Compiled\%NAME%
+set PACKEDDLC=%MODPATH%Compiled\%DLCNAME%\content
 set COMPILED=%MODPATH%Compiled
 set STRINGS=%MODPATH%W3Strings
 set SCRIPTS=%MODPATH%Script
